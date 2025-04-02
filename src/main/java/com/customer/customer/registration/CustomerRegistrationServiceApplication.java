58392f084270b60e67b1e09d08c9711d84700ba3
mahavir-k <mahaviryadav004@gmail.com>
2025-04-02 23:16:03 +0530

    commit services

---
+++ src/main/java/com/customer/customer/registration/CustomerRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.customer.customer.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer/customer/registration/CustomerRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.customer.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
